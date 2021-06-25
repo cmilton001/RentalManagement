51b4210faf0008cfa1d6400760a3a00ee170dea1
kangharo96 <hcmh96@gmail.com>
2021-06-25 02:09:14 -0400

    harrison updated/fixed models.py

--- rentalsite/models.py
+++ rentalsite/models.py
@@ -25,35 +25,55 @@ class Equipment(models.Model):
     # description = models.TextField()
     # price = models.DecimalField(max_digits=4, decimal_places=2)
     # picture = models.ImageField(upload_to='images', db_column='picture', blank=True, null=True)
-    assetid = make = model = serialnum = vendornum = models.CharField(max_length=200)
+    assetid = models.CharField(max_length=200)
+    make = models.CharField(max_length=200)
+    model = models.CharField(max_length=200)
+    serialnum = models.CharField(max_length=200)
+    vendornum = models.CharField(max_length=200)
     category = models.ForeignKey(Page, null=False, on_delete=models.CASCADE)
-    buyrent = models.CharField(max_length=4, choices=['Buy', 'Rent'], default='Buy')
-    returned = models.CharField(max_length=1, choices=['Y', 'N'])
-
-    class Meta:
-        ordering = ['name']
+    buyrent = models.CharField(max_length=4, choices=(('b', 'Buy'), ('r', 'Rent')), default='Buy')
+    returned = models.CharField(max_length=1, choices=(('y', 'Y'), ('n', 'N')))
 
     def publish(self):
         self.save()
 
 
 class Job(models.Model):
-    jobnum = ordernum = assetid = jobdetails = phase = models.CharField(max_length=200)
+    jobnum = models.CharField(max_length=200)
+    ordernum = models.CharField(max_length=200)
+    assetid = models.CharField(max_length=200)
+    jobdetails = models.CharField(max_length=200)
+    phase = models.CharField(max_length=200)
 
 
 class Vendor(models.Model):
-    vendornum = name = phonenum = salesman = ponum = models.CharField(max_length=200)
+    vendornum = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    phonenum = models.CharField(max_length=200)
+    salesman = models.CharField(max_length=200)
+    ponum = models.CharField(max_length=200)
 
 
 class InvoiceDetails(models.Model):
-    invoicenum = ordernum = orderdetails = models.CharField(max_length=200)
+    invoicenum = models.CharField(max_length=200)
+    ordernum = models.CharField(max_length=200)
+    orderdetails = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
 
 class ReturnSlip(models.Model):
-    ordernum = invoicenum = returndate = models.CharField(max_length=200)
+    ordernum = models.CharField(max_length=200)
+    invoicenum = models.CharField(max_length=200)
+    returndate = models.CharField(max_length=200)
 
 
 class OrderMaster(models.Model):
-    ordernum = jobnums = vendornum = assetnum = dateplaced = dateneeded = dateentered = expecteddur = models.CharField(max_length=200)
+    ordernum = models.CharField(max_length=200)
+    jobnums = models.CharField(max_length=200)
+    vendornum = models.CharField(max_length=200)
+    assetnum = models.CharField(max_length=200)
+    dateplaced = models.CharField(max_length=200)
+    dateneeded = models.CharField(max_length=200)
+    dateentered = models.CharField(max_length=200)
+    expecteddur = models.CharField(max_length=200)
 
