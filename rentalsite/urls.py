e81cdb67cf6499856b1cb1967c38dd6219f0dd70
cmilton001 <cynthiamilton95@gmail.com>
2021-06-29 22:49:24 -0400

    - search functions added for all models up till WeeklyReports, except for Categories

--- rentalsite/urls.py
+++ rentalsite/urls.py
@@ -17,30 +17,35 @@ urlpatterns = [
     path('details/<int:pk>/', views.ListDetail.as_view(), name='details'),
     path('details/<int:pk>/update/', views.EquipmentUpdate.as_view(), name='update'),
     ###Job URLS ###
+    path('job_search/', views.JobSearchView.as_view(), name='job_search'),
     path('job_details/<int:pk>/job_list/', views.JobList.as_view(), name='job_list'),
     path('job_details/<int:pk>/', views.JobListDetail.as_view(), name='job_details'),
     path('job_create/', views.JobCreate.as_view(), name='job_create'),
     path('job_details/<int:pk>/job_update/', views.JobUpdate.as_view(), name='job_update'),
     path('job_details/<int:pk>/job_delete/', views.JobDelete.as_view(), name='job_delete'),
     ### Vendor URLS ###
+    path('vendor_search/', views.VendorSearchView.as_view(), name='vendor_search'),
     path('vendor_details/<int:pk>/vendor_list/', views.VendorList.as_view(), name='vendor_list'),
     path('vendor_details/<int:pk>/', views.VendorListDetail.as_view(), name='vendor_details'),
     path('vendor_create/', views.VendorCreate.as_view(), name='vendor_create'),
     path('vendor_details/<int:pk>/vendor_update/', views.VendorUpdate.as_view(), name='vendor_update'),
     path('vendor_details/<int:pk>/vendor_delete/', views.VendorDelete.as_view(), name='vendor_delete'),
     ### InvoiceDetails URLS ###
+    path('invoice_search/', views.InvoiceSearchView.as_view(), name='invoice_search'),
     path('invoice_details/<int:pk>/invoice_list/', views.InvoiceList.as_view(), name='invoice_list'),
     path('invoice_details/<int:pk>/', views.InvoiceListDetail.as_view(), name='invoice_details'),
     path('invoice_create/', views.InvoiceCreate.as_view(), name='invoice_create'),
     path('invoice_details/<int:pk>/invoice_update/', views.InvoiceUpdate.as_view(), name='invoice_update'),
     path('invoice_details/<int:pk>/invoice_delete/', views.InvoiceDelete.as_view(), name='invoice_delete'),
     ### ReturnSlip URLS ###
+    path('returns_search/', views.ReturnsSearchView.as_view(), name='returns_search'),
     path('returns_details/<int:pk>/returns_list/', views.ReturnsList.as_view(), name='returns_list'),
     path('returns_details/<int:pk>/', views.ReturnsListDetail.as_view(), name='returns_details'),
     path('returns_create/', views.ReturnsCreate.as_view(), name='returns_create'),
     path('returns_details/<int:pk>/returns_update/', views.ReturnsUpdate.as_view(), name='returns_update'),
     path('returns_details/<int:pk>/returns_delete/', views.ReturnsDelete.as_view(), name='returns_delete'),
     ### Orders URLS ###
+    path('order_search/', views.OrderSearchView.as_view(), name='order_search'),
     path('order_details/<int:pk>/order_list/', views.OrderList.as_view(), name='order_list'),
     path('order_details/<int:pk>/', views.OrderListDetail.as_view(), name='order_details'),
     path('order_create/', views.OrderCreate.as_view(), name='order_create'),
@@ -49,6 +54,7 @@ urlpatterns = [
     ### Reports page ###
     path('reports_list/', views.reports, name='reports_list'),
     ### Weekly Report URLS ###
+    path('weeklyreport_search/', views.WeeklyReportSearchView.as_view(), name='weeklyreport_search'),
     path('weeklyreport_details/<int:pk>/weeklyreport_list/', views.WeeklyReportList.as_view(),
          name='weeklyreport_list'),
     path('weeklyreport_details/<int:pk>/', views.WeeklyReportListDetail.as_view(), name='weeklyreport_details'),
